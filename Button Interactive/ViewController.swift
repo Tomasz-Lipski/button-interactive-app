b2808888b7fd1ef25f35da9e017571f21b89874e
Tomasz Lipski <tomasz.lipski@gmail.com>
2017-01-17 23:08:38 +0000

    my 1st commit

---
+++ Button Interactive/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Button Interactive
+//
+//  Created by Tomasz Lipski on 09/01/2017.
+//  Copyright © 2017 Tomasz Lipski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+
+    @IBAction func buttonTapped(_ sender: Any) {
+    label.text = textField.text
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
